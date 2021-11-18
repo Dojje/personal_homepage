5124d88adcf5edb6ff746f90f2557e12a099bcb0
dojje <dojje@users.noreply.github.com>
2021-11-18 23:13:45 +0100

    added coilors

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/home.module.scss'
 
-import pfp from '../public/images/pfp.svg'
+// import pfp from '../public/images/pfp.svg'
 
 export default function Home() {
   return (<>
@@ -12,15 +12,17 @@ export default function Home() {
     <div className={styles.hero}>
       <div className={styles.middlepart}>
         <div className={styles.pfp}>
-        <Image
-        src={pfp}
-        alt="cat"
-        />
+          <Image
+            src="https://i.imgur.com/gbJnQDb.png"
+            width="400"
+            height="400"
+            alt="cool cat pfp"
+          />
         </div>
 
         <div className={styles.bio}>
           <p>
-            I LOVE FEMBOYS I FUCKING LOVE THEM
+            Hey guys
           </p>
         </div>
       </div>
@@ -29,16 +31,16 @@ export default function Home() {
     <div className={styles.under}>
       <div className={styles.survival}>
         <p>
-          Daniel has survived {getYears()}&nbsp;days
+          Daniel has survived {getAge()}&nbsp;days
         </p>
       </div>
     </div>
   </>)
 }
 
-function getYears() {
-  var ms_alive = (Date.now() - Date.parse("29 Jan 2007 11:27:00 GMT +0100"));
+function getAge() {
+  var ms_alive = (Date.now() - Date.parse("29 Jan 2007 GMT +0100"));
   var days_alive = ms_alive / 1000 / 60 / 60 / 24;
+
   return Math.floor(days_alive)
 }
-
