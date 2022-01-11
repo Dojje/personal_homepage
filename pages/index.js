552ac691635d7376906e70fa041e62519942c01e
dojje <dojje@users.noreply.github.com>
2022-01-11 18:31:37 +0100

    fixed scaling issue removed hh map maker

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,55 @@
 import styles from '../styles/home.module.scss'
 import { useEffect, useState } from "react";
 
-import React from 'react'
+import React from 'react';
+
+class ThingDone extends React.Component {
+  render() {
+    return(<div className={styles.thingDone}>
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.description}</p>
+      </div>
+      <div>
+        <img src="https://i.ytimg.com/vi/S1cde5JUAuQ/maxresdefault.jpg" alt="" />
+      </div>
+    </div>)
+  }
+}
 
 export default function Home() {
   return (<>
-    <body className={styles.body}></body>
-    <div className={styles.hero}>
-      <div className={styles.middlepart}>
-        <div className={styles.pfp}>
-          {/* eslint-disable-next-line @next/next/no-img-element*/}
-          <img
-            src="https://i.imgur.com/gbJnQDb.png"
-            width="400"
-            height="400"
-            alt="cool cat pfp"
-          />
-            
-        </div>
+    <div className={styles.body}>
+      <div className={styles.hero}>
+        <div className={styles.middlepart}>
+          <div className={styles.pfp}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://i.imgur.com/gbJnQDb.png"
+              width="400"
+              height="400"
+              alt="cool cat pfp"
+            />
 
-        <div className={styles.bio}>
-          <p>
-            Hey guys
-          </p>
-        </div>
-      </div>
-    </div>
+          </div>
 
-
-    <div className={styles.stuffDone}>
-      <h1>Things i have done</h1>
-      <div className={styles.stuffDoneGrid}>
-        <div>
-          <a href="./yunglean/">yung lean</a>
+          <div className={styles.bio}>
+            <p>hey guys</p>
+          </div>
         </div>
       </div>
+
+      <div className={styles.stuffDone}>
+        <h1>Things i have done</h1>
+        <ThingDone title="Yung Lean Jacket" description="Yung lean jacket btw it only works on computer (rip)">
+
+        </ThingDone>
+      </div>
+
+      <footer className={styles.footer}>
+
+      </footer>
     </div>
-
-    <footer className={styles.footer}>
-
-    </footer>
   </>)
 }
 
@@ -53,7 +63,7 @@ function getDaysAlive() {
 function getAge() {
   var ms_alive = (Date.now() - Date.parse("29 Jan 2007 11:27GMT +0100"));
   var age = ms_alive / 1000 / 60 / 60 / 24 / 365
-  
+
   return age
 }
 
