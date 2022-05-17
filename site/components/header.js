ffe087495f0bf0725f389f475ad73ad1a1d6b9f2
dojje <dojje@users.noreply.github.com>
2022-05-17 23:05:18 +0200

    header changes language

--- site/components/header.js
+++ site/components/header.js
@@ -1,12 +1,38 @@
 import styles from '../styles/home.module.scss'
 
-const Header = () => {
+const Header = ({lang, setLang}) => {
+    let getLang = () => {
+        if (lang === "sv") {
+            return "english"
+        } else if (lang === "en") {
+            return "swedish"
+        } else {
+          return "nothing"
+        }
+    }
+    
+    let switchLang = () => {
+        if (lang === "sv") {
+            setLang("en")
+        } else if (lang === "en") {
+            setLang("sv")
+        }
+    }
+
   return (
     <div className={styles.header} >
       <a href="/">
         <h2>Dojjes personal homepage</h2>
       </a>
-      <div>
+        <button 
+            style={{
+                fontSize: "24px",
+                marginLeft: "auto",
+            }}
+            onClick={switchLang}
+            >
+            {getLang()}
+        </button>
         { /*
         TODO Fix links for header
         <a href="/blog">
@@ -16,7 +42,6 @@ const Header = () => {
           contact
         </a>
         */}
-      </div>
     </div>
   )
 }
