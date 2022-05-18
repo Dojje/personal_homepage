6e36d445fccbf2d47cfd121806403c6f1ceb1b48
dojje <dojje@users.noreply.github.com>
2022-05-18 19:50:39 +0200

    fixed header

--- site/components/header.js
+++ site/components/header.js
@@ -2,12 +2,27 @@ import styles from '../styles/home.module.scss'
 import { useEffect } from 'react'
 import Cookie from 'js-cookie'
 
+function LinkButton({lang, href, text}) {
+  return (
+    <a href={href}>
+      <button style={{
+        fontSize: "24px",
+        marginLeft: "auto",
+        padding: "10px 15px",
+        backgroundColor: "var(--accent)",
+      }}>
+        {text}
+      </button>
+    </a>
+  )
+}
+
 const Header = ({lang, setLang}) => {
   let getLang = () => {
     if (lang === "sv") {
-      return "english"
+      return "in english"
     } else if (lang === "en") {
-      return "swedish"
+      return "in swedish"
     } else {
       return "nothing"
     }
@@ -27,28 +42,29 @@ const Header = ({lang, setLang}) => {
   }, [lang])
 
   return (
-    <div className={styles.header} >
+    <div style={{
+      width: "100%",
+      backgroundColor: "var(--background)",
+      padding: "20px 60px",
+
+      display: "grid",
+      gridTemplateColumns: "1fr auto 1fr",
+    }} >
+      <div>
+        <LinkButton text={lang === "en" ? "blog" : "blogg"} href="/blog" />
+      </div>
       <a href="/">
         <h2>{lang == "en" ? "dojjes personal homepage" : "dojjes hemsida"}</h2>
       </a>
-        <button 
-            style={{
-                fontSize: "24px",
-                marginLeft: "auto",
-            }}
-            onClick={switchLang}
-            >
-            {getLang()}
-        </button>
-        { /*
-        TODO Fix links for header
-        <a href="/blog">
-        blog
-        </a>
-        <a href="/contact">
-          contact
-        </a>
-        */}
+      <button 
+        style={{
+          fontSize: "24px",
+          marginLeft: "auto",
+        }}
+        onClick={switchLang}
+        >
+        {getLang()}
+      </button>
     </div>
   )
 }
